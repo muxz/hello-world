d03c82d4711835263c2f390e3a31eea25138617d
Shaun Marcoccia <shaun.marcoccia@pcc.edu>
2019-10-22 11:53:35 -0700

    Created HelloWorld class

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Shaun Marcoccia
+ * Version 10.22.19
+ */
+
 public class HelloWorld {
+    public static void main(String[]args) {
+        System.out.println("Hello world!");
+    }
 }
